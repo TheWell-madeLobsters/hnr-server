efaca2a8d8fcf85e17ee4e48006931571084ba56
Marco Terrinoni <marco.terrinoni90@gmail.com>
2016-05-29 11:01:55 +0200

    Restore connessione DB

--- main.js
+++ main.js
@@ -31,22 +31,17 @@ app.post(SIT_SENSE_ROOT + "/dati_sedile", function(req, res) {
     res.sendStatus(200);
 });
 
-// Creazione della Web app
-app.listen(3000, function () {
-    console.log("SitSense Web App in ascolto sulla porta " + SIT_SENSE_PORT);
-});
+// Connessione Couchbase
+var cluster = new couchbase.Cluster("couchbase://40.69.29.211"); // couchbase e' sulla stessa macchina
+var bucket = cluster.openBucket("sit-sense", function(err) {
+    if(err) {
+        console.error("Errore durante il tentativo di connessione al DB. Messaggio: " + err);
+        process.exit(1);
+    }
+    console.log("Connessione al DB stabilita");
 
-// // Connessione Couchbase
-// var cluster = new couchbase.Cluster("couchbase://40.69.29.211"); // couchbase e' sulla stessa macchina
-// var bucket = cluster.openBucket("sit-sense", function(err) {
-//     if(err) {
-//         console.error("Errore durante il tentativo di connessione al DB. Messaggio: " + err);
-//         process.exit(1);
-//     }
-//     console.log("Connessione al DB stabilita");
-//
-//     // Creazione della Web app
-//     app.listen(3000, function () {
-//         console.log("SitSense Web App in ascolto sulla porta " + SIT_SENSE_PORT);
-//     });
-// });
+    // Creazione della Web app
+    app.listen(3000, function () {
+        console.log("SitSense Web App in ascolto sulla porta " + SIT_SENSE_PORT);
+    });
+});
