3e3f3d788de6f9453f5268a40967d5d8cff37ce7
Marco Terrinoni <marco.terrinoni90@gmail.com>
2016-05-29 03:02:20 +0200

    Messe in commento tutte le istruzioni relative a couchbase, in quanto sul server l'installazione di couchbase tramite npm genera errori

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Inclusione dipendenze
 var express = require('express'); // express framework per web app
 var bodyParser = require('body-parser'); // parser per json
-var couchbase = require("couchbase"); // couchbase driver
+// var couchbase = require("couchbase"); // couchbase driver
 
 // Costanti
 var SIT_SENSE_ROOT = "/sit-sense";
@@ -13,7 +13,9 @@ var SIT_SENSE_PASSWORD = "g2YL8PIGEyeXE8MheKLE"
 var app = express();
 app.use(bodyParser.json()); // per attivare il parsing json
 app.disable('x-powered-by'); // impostazione di sicurezza per header
-app.get(SIT_SENSE_ROOT + "/status", passport.authenticate('basic', { session: false }), function(req, res) {
+
+// Status
+app.get(SIT_SENSE_ROOT + "/status", function(req, res) {
     console.log("Richiesta '/status' ricevuta");
     res.writeHead(200, {"Content-Type": "application/json"});
     var json = JSON.stringify({
@@ -23,22 +25,28 @@ app.get(SIT_SENSE_ROOT + "/status", passport.authenticate('basic', { session: fa
     res.end(json);
 })
 
-app.post(SIT_SENSE_ROOT + "/rcv_dati", passport.authenticate('basic', { session: false }), function(req, res) {
+// Ricezione dati sedile
+app.post(SIT_SENSE_ROOT + "/dati_sedile", function(req, res) {
     console.log("Richiesta ricezione dati ricevuta, contenuto: " + JSON.stringify(req.body));
     res.sendStatus(200);
 });
 
-// Connessione Couchbase
-var cluster = new couchbase.Cluster("couchbase://138.91.61.209"); // couchbase e' sulla stessa macchina
-var bucket = cluster.openBucket("sit-sense", function(err) {
-    if(err) {
-        console.error("Errore durante il tentativo di connessione al DB. Messaggio: " + err);
-        process.exit(1);
-    }
-    console.log("Connessione al DB stabilita");
-
-    // Creazione della Web app
-    app.listen(3000, function () {
-        console.log("SitSense Web App in ascolto sulla porta " + SIT_SENSE_PORT);
-    });
+// Creazione della Web app
+app.listen(3000, function () {
+    console.log("SitSense Web App in ascolto sulla porta " + SIT_SENSE_PORT);
 });
+
+// // Connessione Couchbase
+// var cluster = new couchbase.Cluster("couchbase://138.91.61.209"); // couchbase e' sulla stessa macchina
+// var bucket = cluster.openBucket("sit-sense", function(err) {
+//     if(err) {
+//         console.error("Errore durante il tentativo di connessione al DB. Messaggio: " + err);
+//         process.exit(1);
+//     }
+//     console.log("Connessione al DB stabilita");
+//
+//     // Creazione della Web app
+//     app.listen(3000, function () {
+//         console.log("SitSense Web App in ascolto sulla porta " + SIT_SENSE_PORT);
+//     });
+// });
