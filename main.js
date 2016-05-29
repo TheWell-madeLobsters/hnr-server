9c3a315cce521105d63b5d8e30f42fb9c0ca9b15
Marco Terrinoni <marco.terrinoni90@gmail.com>
2016-05-29 11:16:22 +0200

    reinserimento bozza per ricezione dati sedile

--- main.js
+++ main.js
@@ -9,11 +9,27 @@ var SIT_SENSE_PORT = 3000;
 var SIT_SENSE_USERNAME = "sit-sense";
 var SIT_SENSE_PASSWORD = "g2YL8PIGEyeXE8MheKLE"
 
-// Definizione delle API
+// Definizioni per express
 var app = express();
 app.use(bodyParser.json()); // per attivare il parsing json
 app.disable('x-powered-by'); // impostazione di sicurezza per header
 
+// Connessione Couchbase
+var cluster = new couchbase.Cluster("couchbase://40.69.29.211"); // couchbase e' sulla stessa macchina
+var bucket = cluster.openBucket("sit-sense", function(err) {
+    if(err) {
+        console.error("Errore durante il tentativo di connessione al DB. Messaggio: " + err);
+        process.exit(1);
+    }
+    console.log("Connessione al DB stabilita");
+
+    // Creazione della Web app
+    app.listen(3000, function () {
+        console.log("SitSense Web App in ascolto sulla porta " + SIT_SENSE_PORT);
+    });
+});
+
+// Definizione API
 // Status
 app.get(SIT_SENSE_ROOT + "/status", function(req, res) {
     console.log("Richiesta '/status' ricevuta");
@@ -28,20 +44,13 @@ app.get(SIT_SENSE_ROOT + "/status", function(req, res) {
 // Ricezione dati sedile
 app.post(SIT_SENSE_ROOT + "/dati_sedile", function(req, res) {
     console.log("Richiesta ricezione dati ricevuta, contenuto: " + JSON.stringify(req.body));
-    res.sendStatus(200);
-});
 
-// Connessione Couchbase
-var cluster = new couchbase.Cluster("couchbase://40.69.29.211"); // couchbase e' sulla stessa macchina
-var bucket = cluster.openBucket("sit-sense", function(err) {
-    if(err) {
-        console.error("Errore durante il tentativo di connessione al DB. Messaggio: " + err);
-        process.exit(1);
-    }
-    console.log("Connessione al DB stabilita");
+    bucket.insert(req.body.timestamp + "-" + req.body.user_id, req.body, function(err, res) {
+        if(err) {
+            console.error("Errore durante l'inserimento di un oggetto su DB. Messaggio: " + err);
+            res.sendStatus(500);
+        }
 
-    // Creazione della Web app
-    app.listen(3000, function () {
-        console.log("SitSense Web App in ascolto sulla porta " + SIT_SENSE_PORT);
+        res.sendStatus(200);
     });
 });
