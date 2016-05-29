83356f7adb7277deb2b7c975c4f6177691d302ed
Marco Terrinoni <marco.terrinoni90@gmail.com>
2016-05-29 02:52:18 +0200

    Creazione di una struttura base per il server

---
+++ main.js
@@ -0,0 +1,44 @@
+// Inclusione dipendenze
+var express = require('express'); // express framework per web app
+var bodyParser = require('body-parser'); // parser per json
+var couchbase = require("couchbase"); // couchbase driver
+
+// Costanti
+var SIT_SENSE_ROOT = "/sit-sense";
+var SIT_SENSE_PORT = 3000;
+var SIT_SENSE_USERNAME = "sit-sense";
+var SIT_SENSE_PASSWORD = "g2YL8PIGEyeXE8MheKLE"
+
+// Definizione delle API
+var app = express();
+app.use(bodyParser.json()); // per attivare il parsing json
+app.disable('x-powered-by'); // impostazione di sicurezza per header
+app.get(SIT_SENSE_ROOT + "/status", passport.authenticate('basic', { session: false }), function(req, res) {
+    console.log("Richiesta '/status' ricevuta");
+    res.writeHead(200, {"Content-Type": "application/json"});
+    var json = JSON.stringify({
+        status: "OK",
+        msg: "Aho', ecchime!"
+    });
+    res.end(json);
+})
+
+app.post(SIT_SENSE_ROOT + "/rcv_dati", passport.authenticate('basic', { session: false }), function(req, res) {
+    console.log("Richiesta ricezione dati ricevuta, contenuto: " + JSON.stringify(req.body));
+    res.sendStatus(200);
+});
+
+// Connessione Couchbase
+var cluster = new couchbase.Cluster("couchbase://138.91.61.209"); // couchbase e' sulla stessa macchina
+var bucket = cluster.openBucket("sit-sense", function(err) {
+    if(err) {
+        console.error("Errore durante il tentativo di connessione al DB. Messaggio: " + err);
+        process.exit(1);
+    }
+    console.log("Connessione al DB stabilita");
+
+    // Creazione della Web app
+    app.listen(3000, function () {
+        console.log("SitSense Web App in ascolto sulla porta " + SIT_SENSE_PORT);
+    });
+});
